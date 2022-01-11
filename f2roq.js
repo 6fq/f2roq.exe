e3a992b77500d0240ee5e009f730f56090d7996e
Oncl f2roq <97564562+6fq@users.noreply.github.com>
2022-01-12 01:33:33 +0200

    Create f2roq.js

---
+++ f2roq.js
@@ -0,0 +1,11 @@
+const f2roq = {
+ name: "f2roq",
+  age: 18,
+  languages: ["Javascript", "HTML" , "CSS"],
+projects: ({
+         name: "khalyd", 
+   link: "https://discord.gg/JQDPkMQcMd" 
+  }] 
+ }  
+console.log(f2roq);
+}
